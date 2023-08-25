903bf9d7c3710e859dab8b2ff19b6f7cbf24d33d
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-08-25 15:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 // This is the boilerplate code given for you
 // You can modify this code
 // Product data
+document.addEventListener("DOMContentLoaded", () => {
+  const productList = document.getElementById("product-list");
+  const cart = document.getElementById("cart");
+
+  // Initialize cart from session storage
+  const cartItems = JSON.parse(sessionStorage.getItem("cart")) || [];
+  cartItems.forEach(item => addToCart(item));
+
+  // Add event listeners to product buttons
+  const addToCartButtons = document.querySelectorAll(".add-to-cart");
+  addToCartButtons.forEach(button => {
+    button.addEventListener("click", addToCartHandler);
+  });
+
+  function addToCartHandler(event) {
+    const product = event.target.parentElement;
+    const productId = product.getAttribute("data-id");
+    const productName = product.textContent.split("-")[0].trim();
+    const cartItem = { id: productId, name: productName };
+
+    addToCart(cartItem);
+    updateCartStorage();
+  }
+
+  function addToCart(item) {
+    const li = document.createElement("li");
+    li.textContent = item.name;
+    cart.appendChild(li);
+  }
+
+  function updateCartStorage() {
+    const cartItems = Array.from(cart.children).map(li => ({
+      id: li.getAttribute("data-id"),
+      name: li.textContent
+    }));
+    sessionStorage.setItem("cart", JSON.stringify(cartItems));
+  }
+});
+
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
